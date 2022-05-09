723129206b308bc890c949d60cd8782c5f91d6d9
Vladlen Schetinin <vvschetinin@gmail.com>
2022-05-09 21:41:37 +0300

    Цепочки методов и свойств. Начало

--- oopwm/intro/methodchains/index.php
+++ oopwm/intro/methodchains/index.php
@@ -1,6 +1,6 @@
 <?php $root = $_SERVER['DOCUMENT_ROOT'];?>
 <?php require_once $root . '/template/header.php';?>
-
+<?php require_once 'classes/Car.php';?>
 
 
 <?php require_once $root . '/template/footer.php';?>
