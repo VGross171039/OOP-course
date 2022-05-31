0c86a3dce6b9bd1b42d7f52edcbadd9434280e03
Vladlen Schetinin <born-1965@mail.ru>
2022-06-01 00:23:37 +0300

    Clear files

--- oopwm/intro/constructor/index.php
+++ oopwm/intro/constructor/index.php
@@ -2,8 +2,6 @@
 <?php require_once $root . '/template/header.php';?>
 <?php require_once 'classes/Car.php';?>
 
-<h3>Работа конструктора</h3>
-
 
 
 <?php require_once $root . '/template/footer.php';?>
--- oopwm/intro/methodchains/index.php
+++ oopwm/intro/methodchains/index.php
@@ -2,12 +2,6 @@
 <?php require_once $root . '/template/header.php';?>
 <?php require_once 'classes/Car.php';?>
 
-<?php 
-$bmw = new Car();
-$tank = $bmw -> fill(10) -> ride(38) -> tank;
 
-echo $tank;
-
-?>
 
 <?php require_once $root . '/template/footer.php';?>
