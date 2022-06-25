48ea47a824c23203d021f61b70e6f49a132b8f7b
Vladlen Schetinin <vvschetinin@gmail.com>
2022-06-25 18:18:41 +0300

    Модификаторы доступа

--- oopwm/one/modifiers/classes/Car.php
+++ oopwm/one/modifiers/classes/Car.php
@@ -1,10 +1,40 @@
 <?php 
+class CarTest{
+  public $name;
+  protected $color;
+  private $disk;
+
+}
+
 class Car{
   private $model;
 
   public function setModel($model)
   {
     $this->model = $model;
+  }
+
+  public function getModel()
+  {
+    return 'Car model: ' . $this->model . '<br>';
+  }
+
+}
+
+class CarSelect{
+  private $model;
+
+  public function setModel($model)
+  {
+    // Устанавливаем модели автомобилей, которые могут быть назначены свойству $model в виде массива
+    $selectModel = array('Mersedes-Benz', 'BMW', 'Opel');
+
+    if(in_array($model, $selectModel)){
+      $this->model = $model;
+    } else {
+      $this->model = ' - нет в нашем списке моделей!';
+    }
+
   }
 
   public function getModel()
--- oopwm/one/modifiers/index.php
+++ oopwm/one/modifiers/index.php
@@ -2,10 +2,30 @@
 <?php require_once $root . '/template/header.php';?>
 <?php require_once 'classes/Car.php';?>
 
-<h3>Модификаторы доступа в PHP</h3>
+<h3 class="pb-05">Модификаторы доступа в PHP</h3>
+<p>Задают область видимости для свойств и методов класса</p>
 <p>Доступ к свойству private:</p>
 Сеттеры — методы устанавливающие значения приватных свойств. <br>
 Геттеры — методы, которые получают значения частных свойств. <br>
+
+<ul class="my-05">
+  <li><span class="accent">public</span>— свойство или метод доступны отовсюду. Это значение по умолчанию</li>
+  <li><span class="accent">protected</span>— к свойству или методу можно получить доступ внутри класса и классов,
+    производных от этого класса</li>
+  <li><span class="accent">private</span>— свойство или метод могут быть доступны ТОЛЬКО внутри класса</li>
+</ul>
+
+<hr>
+
+<?php 
+$testCar = new CarTest();
+
+$testCar->name = 'Жигули';
+// $testCar->color = 'Yellow'; // ERROR
+// $testCar->disk = 'Stamped'; // ERROR
+
+print_arr($testCar);
+?>
 
 <hr>
 
@@ -16,6 +36,19 @@ $bmw->setModel('BMW');
 echo $bmw->getModel();
 
 print_arr($bmw);
+?>
+
+<hr>
+
+<?php 
+$mersedes = new CarSelect();
+$vaz = new CarSelect();
+
+$mersedes->setModel('Mersedes-Benz');
+echo $mersedes->getModel();
+
+$vaz->setModel('VAZ-2107');
+echo $vaz->getModel();
 
 ?>
 
