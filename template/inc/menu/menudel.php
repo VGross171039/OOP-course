422afd6866328ae7cb2260691b79ee5bc6723af6
Vladlen Schetinin <vvschetinin@gmail.com>
2022-07-13 15:46:12 +0300

    Теория и синтаксис

--- template/inc/menu/menudel.php
+++ template/inc/menu/menudel.php
@@ -1,15 +1,21 @@
 <h2>Course DEl</h2>
 
-<ul class="content-list">
+<ul class="content-list listdel">
   <li>
-    <p class="separator">Section Intro</p>
+    <p class="separator">Intro</p>
   </li>
-  <li><a href="/oopdel/intro/history/">История вопроса и принципы</a></li>
-  <li><a href="">2</a></li>
-  <li><a href="">3</a></li>
-  <li><a href="">4</a></li>
-  <li class="pb-05"><a href="">5</a></li>
+  <li class="pb-05"><a href="/oopdel/intro/history/">История вопроса и принципы</a></li>
   <li>
     <hr>
+    <p class="separator">Теория и синтаксис. Как пишутся классы</p>
+  </li>
+  <li><a href="/oopdel/syntax/1/">1</a></li>
+  <li><a href="/oopdel/syntax/2/">2</a></li>
+  <li><a href="/oopdel/syntax/3/">3</a></li>
+  <li><a href="/oopdel/syntax/4/">4</a></li>
+  <li class="pb-05"><a href="/oopdel/syntax/5/">5</a></li>
+  <li>
+    <hr>
+    <p class="separator">Section 2</p>
   </li>
 </ul>
